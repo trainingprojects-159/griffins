0e258fed27be6012b3a5c349d2861085bd4c541a
kuppani.dinesh <kuppani.dinesh@WKSBAN24TR17013.corp.mphasis.com>
2019-05-08 12:11:10 +0530

    packages push

---
+++ Griffin_Airways/src/main/java/com/mphasis/griffin/serviceImpl/Flight.java
@@ -0,0 +1,58 @@
+package com.mphasis.griffin.serviceImpl;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+
+@Entity
+public class Flight {
+@Id
+
+
+@GeneratedValue(strategy=GenerationType.SEQUENCE)
+ private int flightId;
+ private String fname;
+ private int reserveSeat;
+ private int seatCap;
+ @ManyToOne
+ private Route route;
+ @OneToMany(mappedBy="flight")
+ 
+ 
+public void setRoute(Route route) {
+	this.route = route;
+}
+public int getFlightId() {
+	return flightId;
+}
+public void setFlightId(int flightId) {
+	this.flightId = flightId;
+}
+public String getFname() {
+	return fname;
+}
+public void setFname(String fname) {
+	this.fname = fname;
+}
+public int getReserveSeat() {
+	return reserveSeat;
+}
+public void setReserveSeat(int reserveSeat) {
+	this.reserveSeat = reserveSeat;
+}
+public int getSeatCap() {
+	return seatCap;
+}
+public void setSeatCap(int seatCap) {
+	this.seatCap = seatCap;
+}
+@Override
+public String toString() {
+	return "Flight [flightId=" + flightId + ", fname=" + fname + ", reserveSeat=" + reserveSeat + ", seatCap=" + seatCap
+			+ "]";
+}
+
+}
