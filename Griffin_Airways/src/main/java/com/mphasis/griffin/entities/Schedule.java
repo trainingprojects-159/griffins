e97f6000f8e4f2ddac12b9e04906cc966e65665d
tanya.shanker <tanya.shanker@WKSBAN24TR17008.corp.mphasis.com>
2019-05-08 15:28:44 +0530

    done the schedule relations

--- Griffin_Airways/src/main/java/com/mphasis/griffin/entities/Schedule.java
+++ Griffin_Airways/src/main/java/com/mphasis/griffin/entities/Schedule.java
@@ -4,6 +4,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 
 @Entity
@@ -11,12 +12,12 @@ public class Schedule {
 	@Id
 	@GeneratedValue(strategy=GenerationType.SEQUENCE)
 	private int scheId;
-	private String flightId;
-	private String routeId;
 	private String scheDate;
 	@ManyToOne
+@JoinColumn(name="flightId")
 	private Flight flight;
 	@ManyToOne
+	@JoinColumn(name="routeId")
 	private Route route;
 	
 	public Route getRoute() {
