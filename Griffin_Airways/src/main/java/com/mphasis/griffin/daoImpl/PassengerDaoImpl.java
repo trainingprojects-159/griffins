8b9784aba85b3307669334f2b7df6b6172c3adf3
Chhavi1234 <49522241+Chhavi1234@users.noreply.github.com>
2019-05-09 10:39:13 +0530

    Add files via upload
    passenger dao impl

---
+++ Griffin_Airways/src/main/java/com/mphasis/griffin/daoImpl/PassengerDaoImpl.java
@@ -0,0 +1,61 @@
+package com.mphasis.griffin.daoImpl;
+
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.mphasis.griffin.dao.PassengerDao;
+import com.mphasis.griffin.entities.PassengerInfo;
+
+public class PassengerDaoImpl implements PassengerDao{
+@Autowired
+SessionFactory sessionFactory;
+	public void insertPassenger(PassengerInfo p) {
+		Session session=sessionFactory.openSession();
+		Transaction tr=session.beginTransaction();
+		session.save(p);
+		tr.commit();
+		
+	}
+
+	public void updatePassenger(PassengerInfo p) {
+		Session session=sessionFactory.openSession();
+		Transaction tr=session.beginTransaction();
+		session.update(p);
+		tr.commit();
+		
+	}
+
+	public void deletePassenger(int passId) {
+		Session session=sessionFactory.openSession();
+		Transaction tr=session.beginTransaction();
+		PassengerInfo p=(PassengerInfo)session.get(PassengerInfo.class,passId);
+		session.delete(p);
+		tr.commit();
+		
+	}
+
+	public List<PassengerInfo> getAllPassenger() {
+		Session session=sessionFactory.openSession();
+		Transaction tr=session.beginTransaction();
+		List<PassengerInfo> passenger=session.createCriteria(PassengerInfo.class).list();
+		tr.commit();
+		return passenger;
+
+		
+	}
+
+	public PassengerInfo getPassengerById(int passId) {
+		Session session=sessionFactory.openSession();
+		Transaction tr=session.beginTransaction();
+		PassengerInfo p=(PassengerInfo)session.get(PassengerInfo.class,passId);
+	
+		tr.commit();
+		return p;
+		
+	}
+
+}
