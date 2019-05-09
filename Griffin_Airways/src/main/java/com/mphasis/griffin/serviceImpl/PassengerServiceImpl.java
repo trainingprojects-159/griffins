967d16bfb1449fd5944d50e88871d811aad9ccce
Chhavi1234 <49522241+Chhavi1234@users.noreply.github.com>
2019-05-09 10:44:02 +0530

    Add files via upload
    passenger service added

---
+++ Griffin_Airways/src/main/java/com/mphasis/griffin/serviceImpl/PassengerServiceImpl.java
@@ -0,0 +1,47 @@
+package com.mphasis.griffin.serviceImpl;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.mphasis.griffin.dao.PassengerDao;
+import com.mphasis.griffin.entities.PassengerInfo;
+import com.mphasis.griffin.service.PassengerService;
+
+
+@Service
+public class PassengerServiceImpl implements PassengerService {
+@Autowired
+   PassengerDao passengerDao;
+
+
+	public PassengerDao getPassengerDao() {
+	return passengerDao;
+}
+	public void addPassenger(PassengerInfo passenger) {
+		  passengerDao.insertPassenger(passenger);
+		
+	}
+
+	public void updatePassenger(PassengerInfo passenger) {
+		passengerDao.updatePassenger(passenger);
+		
+	}
+
+	public void removePassenger(int passId) {
+		passengerDao.deletePassenger(passId);
+		
+	}
+
+	public PassengerInfo getPassengerById(int passId) {
+		
+		return passengerDao.getPassengerById(passId);
+	}
+
+	public List<PassengerInfo> getAllPassenger() {
+	
+		return passengerDao.getAllPassenger();
+	}
+
+}
