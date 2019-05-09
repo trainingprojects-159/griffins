36b26c6b046da9c3e859f557667703cd278aa880
kuppani.dinesh <kuppani.dinesh@WKSBAN24TR17013.corp.mphasis.com>
2019-05-09 11:07:53 +0530

    class name changed

--- Griffin_Airways/src/main/java/com/mphasis/griffin/entities/Customers.java
+++ Griffin_Airways/src/main/java/com/mphasis/griffin/entities/Customers.java
@@ -6,7 +6,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
-public class Customer {
+public class Customers {
 	@Id
 	@GeneratedValue(strategy=GenerationType.SEQUENCE)
 	private String userId;
